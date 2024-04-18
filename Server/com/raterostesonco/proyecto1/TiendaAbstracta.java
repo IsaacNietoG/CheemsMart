29e9b54312470647fb49a75d8d0ac1bd7837b5f3
MrTaiChi <isaaniet@gmail.com>
2024-04-17 21:28:31 -0600

    feat: Pedillos

--- Server/com/raterostesonco/proyecto1/TiendaAbstracta.java
+++ Server/com/raterostesonco/proyecto1/TiendaAbstracta.java
@@ -1,7 +1,11 @@
 package Server.com.raterostesonco.proyecto1;
 
+import java.util.Iterator;
+
+import Server.com.raterostesonco.proyecto1.basedatos.BankAccount;
 import Server.com.raterostesonco.proyecto1.basedatos.CatalogoItem;
 import Server.com.raterostesonco.proyecto1.basedatos.Cliente;
+import Server.com.raterostesonco.proyecto1.basedatos.NumeroDeCuentaInvalidoException;
 
 /**
  * La clase abstracta para Tiendas del lado del servidor
@@ -27,13 +31,36 @@ public abstract class TiendaAbstracta implements Tienda{
 
     @Override
     public void agregarCarrito(Cliente cliente, CatalogoItem item) {
-        // TODO Auto-generated method stub
+
 
     }
 
+    /**
+     *  Realiza una compra en nombre del cliente indicado, por el carrito
+     *  de compras que tiene actualmente.
+     *
+     *  @param cliente   El cliente que realiza la compra
+     *         cuenta    La cuenta de banco que proporciona el cliente,
+     *                   será la que se va a verificar
+     *
+     *  @return si la compra fue exitosa o no, pudo no haberlo sido por
+     *          un numero de cuenta incorrecto o por fondos insuficientes
+     *  */
     @Override
-    public void hacerCompra(Cliente cliente) {
-        // TODO Auto-generated method stub
+    public boolean hacerCompra(Cliente cliente, String cuenta){
+        BankAccount cuentaBanco;
+        try {
+            cuentaBanco = cliente.getBankAccount(cuenta);
+        } catch (NumeroDeCuentaInvalidoException e) {
+            return false;
+        }
+        Iterator<CatalogoItem> iterador = cliente.getCarritoCompras().darIterador();
+        double cobro;
+        while(iterador.hasNext()){
+            CatalogoItem item = iterador.next();
+            cobro += item.getPrecio();
+        }
+        return cuentaBanco.cobrar(cobro);
 
     }
 }
