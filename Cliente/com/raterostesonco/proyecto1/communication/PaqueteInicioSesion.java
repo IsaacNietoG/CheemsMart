3a67702b1fc0634d6b31b60c433f0a1278089013
DystopianRescuer <diegobravis20@gmail.com>
2024-04-19 01:45:35 -0600

    User-side stuff implemented

---
+++ Cliente/com/raterostesonco/proyecto1/communication/PaqueteInicioSesion.java
@@ -0,0 +1,10 @@
+package Cliente.com.raterostesonco.proyecto1.communication;
+
+public class PaqueteSesion extends Paquete {
+
+    protected PaqueteSesion(String token, String tipo, String email, String contrasenia) {
+        super(token, tipo, args);
+    }
+
+    // INICIO SESION Y CERRAR SESION Y PREGUNTAR SI SESION ACTIVA
+}
--- Cliente/com/raterostesonco/proyecto1/communication/PaqueteSesion.java
+++
@@ -1,6 +0,0 @@
-package Cliente.com.raterostesonco.proyecto1.communication;
-
-public class PaqueteSesion {
-
-    // INICIO SESION Y CERRAR SESION Y PREGUNTAR SI SESION ACTIVA
-}
